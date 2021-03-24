32894c1c890c4f69382ecbdd43f97e1c7f3d7b11
Matt Hale <mhale@factharbor.com>
2021-03-23 20:17:26 -0400

    Add DOM and JS variables to JS file and created containers in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+// DOM variables
+var questionElement = document.getElementById("question");
+var answersElement = document.getElementById("answers");
+
+// JS variables
+var questionArray = [
+    {
+        text: "Inside which HTML element do we put the Javascript?",
+        answers: ["<javascript", "<script>", "<scripting>", "<js>"]
+    },
+    {
+        text: "What is the correct place to insert a JavaScript?",
+        options: ["Both thea <head> and <body> sections are correct", "The <body> section", "The <head> section", "After the <body>"]
+    },
+    {
+        text: "What is the correct syntax referring to a script callled 'xxx.js'",
+        options: ["<script src='xxx.js'>", "<script href='xxx.js'", "<script rel='xxx.s'", "<script link='xxx.js'"]
+    },
+    {
+        text: "How do you write 'Hello World' in an alert box?",
+        options: ["msg('Hello World');", "alert('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');"]
+    },
+    {
+        text: "How do you write an IF statement in JavaScript?",
+        options: ["if i==5 then", "if (i==5)", "if i=5", "if i=5 then"]
+    },
+];
+
+// Function definitions
+
+
+// Event listeners
+
+
+// Function calls
